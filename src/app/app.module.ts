0decf386c18c4ee5eae8ff2d300df07cb3f995ab
Tiago Silva <tiagobsb84@gmail.com>
2023-02-18 07:39:54 -0300

    Implementado o componente footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componente/cabecalho/cabecalho.component';
+import { FooterComponent } from './componente/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
