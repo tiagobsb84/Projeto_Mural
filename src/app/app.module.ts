885fb0fda25a58c0c6ffe65c88fcc4899d9c3f54
Tiago Silva <tiagobsb84@gmail.com>
2023-02-18 23:13:59 -0300

    Passando o fluxo de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,17 +7,20 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componente/cabecalho/cabecalho.component';
 import { FooterComponent } from './componente/footer/footer.component';
 import { CriarPensamentoComponent } from './componente/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componente/listar-pensamento/listar-pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
     FooterComponent,
-    CriarPensamentoComponent
+    CriarPensamentoComponent,
+    ListarPensamentoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componente/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componente/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css']
 })
-export class CriarPensamentoComponent {
+export class CriarPensamentoComponent implements OnInit {
 
+  pensamentos = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'DEV',
+    modelo: 'modulo1'
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+  criarPensamento() {
+    alert('Criado pensamento');
+  }
+
+  cancelarPensamento() {
+    alert('cancelamento pensamento');
+  }
 }
