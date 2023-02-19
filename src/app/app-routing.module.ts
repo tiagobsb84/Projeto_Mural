1269a4507e2a360c75d1f1473c909d974a247174
Tiago Silva <tiagobsb84@gmail.com>
2023-02-19 00:31:33 -0300

    Navegando com rotiamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarPensamentoComponent } from './componente/listar-pensamento/listar-pensamento.component';
+import { CriarPensamentoComponent } from './componente/pensamentos/criar-pensamento/criar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listarPensamento',
+    pathMatch: "full"
+  },
+  {
+    path: 'criarPensamento',
+    component: CriarPensamentoComponent
+  },
+  {
+    path: 'listarPensamento',
+    component: ListarPensamentoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
