e3925248a488680f4fb42e052c4a47829b0963e9
Tiago Silva <tiagobsb84@gmail.com>
2023-02-27 09:20:33 -0300

    Adicionando titulo dinamicamente e finalizando projeto

--- src/app/componente/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componente/listar-pensamento/listar-pensamento.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../Pensamento';
 import { PensamentoService } from '../pensamentos/pensamento.service';
@@ -22,7 +23,9 @@ export class ListarPensamentoComponent implements OnInit {
 
   listaFavoritos: Pensamento[] = [];
 
-  constructor(private service: PensamentoService) {}
+  titulo: string = 'Meu Mural';
+
+  constructor(private service: PensamentoService, private router: Router) {}
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((resposta) => {
@@ -47,7 +50,17 @@ export class ListarPensamentoComponent implements OnInit {
     })
   }
 
+  recarregarComponente() {
+    this.titulo;
+    this.favoritos = false;
+    this.paginaAtual = 1;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([this.router.url]);
+  }
+
   listarFavoritos() {
+    this.titulo = 'Meus Favoritos';
     this.favoritos = true;
     this.haMaisPensamentos = true;
     this.paginaAtual = 1;
